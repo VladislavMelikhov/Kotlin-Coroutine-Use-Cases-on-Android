1f400639b6b556dc188c1073bcd564d535d152ef
Uladzislau Melikhau <uladzislau.melikhau@consultant.turkcell.com.tr>
2021-03-18 00:25:09 +0300

    [Module-8] Using withContext for context switching

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase10/CalculationInBackgroundViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase10/CalculationInBackgroundViewModel.kt
@@ -2,7 +2,9 @@ package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase1
 
 import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.math.BigInteger
 import kotlin.system.measureTimeMillis
 
@@ -20,18 +22,21 @@ class CalculationInBackgroundViewModel : BaseViewModel<UiState>() {
 
             val resultString: String
             val stringConversionDuration = measureTimeMillis {
-                resultString = result.toString()
+                resultString = withContext(Dispatchers.Default) {
+                    result.toString()
+                }
             }
 
             uiState.value = UiState.Success(resultString, computationDuration, stringConversionDuration)
         }
     }
 
-    private fun calculateFactorialOf(number: Int): BigInteger {
-        var factorial = BigInteger.ONE
-        for (i in 1..number) {
-            factorial = factorial.multiply(BigInteger.valueOf(i.toLong()))
+    private suspend fun calculateFactorialOf(number: Int): BigInteger =
+        withContext(Dispatchers.Default) {
+            var factorial = BigInteger.ONE
+            for (i in 1..number) {
+                factorial = factorial.multiply(BigInteger.valueOf(i.toLong()))
+            }
+            factorial
         }
-        return factorial
-    }
 }
