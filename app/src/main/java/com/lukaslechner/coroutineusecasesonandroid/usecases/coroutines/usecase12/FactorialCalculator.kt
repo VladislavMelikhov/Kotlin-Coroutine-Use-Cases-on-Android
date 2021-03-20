5f15d78d6891f96ec504fb5ec4b7242cbdf8fa0b
Uladzislau Melikhau <uladzislau.melikhau@consultant.turkcell.com.tr>
2021-03-20 11:53:57 +0300

    [Module-8] Implement UseCase12

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase12/FactorialCalculator.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase12/FactorialCalculator.kt
@@ -1,34 +1,29 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase12
 
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.*
 import java.math.BigInteger
 
 class FactorialCalculator(
     private val defaultDispatcher: CoroutineDispatcher = Dispatchers.Default
 ) {
 
-    fun calculateFactorial(
+    suspend fun calculateFactorial(
         factorialOf: Int,
         numberOfCoroutines: Int
-    ): BigInteger {
-
-        // TODO: create sub range list *on background thread*
-        val subRanges = createSubRangeList(factorialOf, numberOfCoroutines)
-
-
-        // TODO: calculate factorial of each subrange in separate coroutine
-        // use calculateFactorialOfSubRange(subRange) therefore
-
-
-        // TODO: create factorial result by multiplying all sub-results and return this
-        // result
-
-        return BigInteger.ZERO
-    }
+    ): BigInteger =
+        withContext(defaultDispatcher) {
+            createSubRangeList(factorialOf, numberOfCoroutines)
+                .map { subRange ->
+                    async {
+                        calculateFactorialOfSubRange(subRange)
+                    }
+                }
+                .awaitAll()
+                .fold(BigInteger.ONE, BigInteger::multiply)
+        }
 
     // TODO: execute on background thread
-    fun calculateFactorialOfSubRange(
+    private fun calculateFactorialOfSubRange(
         subRange: SubRange
     ): BigInteger {
         var factorial = BigInteger.ONE
@@ -38,7 +33,7 @@ class FactorialCalculator(
         return factorial
     }
 
-    fun createSubRangeList(
+    private fun createSubRangeList(
         factorialOf: Int,
         numberOfSubRanges: Int
     ): List<SubRange> {
