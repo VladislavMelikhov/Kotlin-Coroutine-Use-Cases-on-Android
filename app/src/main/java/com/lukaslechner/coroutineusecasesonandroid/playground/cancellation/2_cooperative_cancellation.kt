5806ffeea50e00bcf6b19f483e888ef3c9744b9d
Uladzislau Melikhau <uladzislau.melikhau@consultant.turkcell.com.tr>
2021-03-28 19:57:49 +0300

    [Module-10] Cooperative cancellation

---
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/cancellation/2_cooperative_cancellation.kt
@@ -0,0 +1,23 @@
+package com.lukaslechner.coroutineusecasesonandroid.playground.cancellation
+
+import kotlinx.coroutines.*
+
+fun main() = runBlocking<Unit> {
+
+    val job = launch(Dispatchers.Default) {
+
+        repeat(10) { index ->
+            if (isActive) {
+                println("operation number $index")
+                Thread.sleep(100)
+            } else {
+                println("Cleaning up ... ")
+                throw CancellationException()
+            }
+        }
+    }
+
+    delay(250)
+    println("Cancelling Coroutine")
+    job.cancel()
+}
