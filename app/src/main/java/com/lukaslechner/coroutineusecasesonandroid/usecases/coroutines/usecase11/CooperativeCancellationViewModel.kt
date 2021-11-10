165ae9dc4ec3b5a3046a0b7fb7a1ce4a3d61a0fc
Uladzislau Melikhau <vladislavmelikhov@gmail.com>
2021-11-10 11:48:39 +0300

    [Module-10] UseCase11 Cooperative regarding cancellation

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase11/CooperativeCancellationViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase11/CooperativeCancellationViewModel.kt
@@ -3,19 +3,58 @@ package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase1
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.*
+import timber.log.Timber
+import java.math.BigInteger
+import kotlin.system.measureTimeMillis
 
 class CooperativeCancellationViewModel(
     private val defaultDispatcher: CoroutineDispatcher = Dispatchers.Default
 ) : ViewModel() {
 
-    fun performCalculation(factorialOf: Int) {
+    private var calculationJob: Job? = null
 
+    fun performCalculation(factorialOf: Int) {
+        uiState.value = UiState.Loading
+
+        calculationJob = viewModelScope.launch {
+
+            val result: BigInteger
+            val computationDuration = measureTimeMillis {
+                result = calculateFactorialOf(factorialOf)
+            }
+
+            val resultString: String
+            val stringConversionDuration = measureTimeMillis {
+                resultString = withContext(Dispatchers.Default) {
+                    result.toString()
+                }
+            }
+
+            uiState.value = UiState.Success(resultString, computationDuration, stringConversionDuration)
+        }
+
+        calculationJob?.invokeOnCompletion { cause: Throwable? ->
+            if (cause is CancellationException) {
+                Timber.d("Calculation was cancelled!")
+            }
+        }
     }
 
-    fun cancelCalculation() {
+    private suspend fun calculateFactorialOf(number: Int): BigInteger =
+        withContext(Dispatchers.Default) {
+            var factorial = BigInteger.ONE
+            for (i in 1..number) {
+                ensureActive()
+                factorial = factorial.multiply(BigInteger.valueOf(i.toLong()))
+            }
+            Timber.d("Calculating Factorial Completed!")
+            factorial
+        }
 
+    fun cancelCalculation() {
+        calculationJob?.cancel()
     }
 
     fun uiState(): LiveData<UiState> = uiState
