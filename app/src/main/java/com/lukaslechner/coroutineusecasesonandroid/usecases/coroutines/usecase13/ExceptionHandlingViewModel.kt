3d63d2736ab9ce31aa889a3ead58e4afc21632a3
Uladzislau Melikhau <vladislavmelikhov@gmail.com>
2021-11-11 12:20:09 +0300

    [Module-11] UseCase13 with try-catch

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase13/ExceptionHandlingViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase13/ExceptionHandlingViewModel.kt
@@ -1,16 +1,32 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase13
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
 import kotlinx.coroutines.*
-import timber.log.Timber
+import retrofit2.HttpException
 
 class ExceptionHandlingViewModel(
     private val api: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun handleExceptionWithTryCatch() {
+        uiState.value = UiState.Loading
 
+        viewModelScope.launch {
+            try {
+                api.getAndroidVersionFeatures(27)
+            } catch (exception: Exception) {
+                if (exception is HttpException) {
+                    if (exception.code() == 500) {
+                        // Error Message 1
+                    } else {
+                        // Error Message 2
+                    }
+                }
+                uiState.value = UiState.Error("Network request failed: $exception")
+            }
+        }
     }
 
     fun handleWithCoroutineExceptionHandler() {
