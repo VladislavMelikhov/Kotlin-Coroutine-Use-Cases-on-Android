284003e90ab748a7cd1ab39544c8c5ea912fe971
Uladzislau Melikhau <vladislavmelikhov@gmail.com>
2021-11-11 16:29:18 +0300

    [Module-11] UseCase13 show results even if a child coroutine fails

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase13/ExceptionHandlingViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase13/ExceptionHandlingViewModel.kt
@@ -5,6 +5,7 @@ import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
 import kotlinx.coroutines.*
 import retrofit2.HttpException
+import timber.log.Timber
 
 class ExceptionHandlingViewModel(
     private val api: MockApi = mockApi()
@@ -42,6 +43,50 @@ class ExceptionHandlingViewModel(
     }
 
     fun showResultsEvenIfChildCoroutineFails() {
+        uiState.value = UiState.Loading
 
+        viewModelScope.launch {
+
+            supervisorScope {
+                val oreoFeaturesDeferred = async {
+                    api.getAndroidVersionFeatures(27)
+                }
+
+                val pieFeaturesDeferred = async {
+                    api.getAndroidVersionFeatures(28)
+                }
+
+                val android10FeaturesDeferred = async {
+                    api.getAndroidVersionFeatures(29)
+                }
+
+                val oreoFeatures = try {
+                    oreoFeaturesDeferred.await()
+                } catch (e: Exception) {
+                    Timber.e("Error loading oreo features $e")
+                    null
+                }
+
+                val pieFeatures = try {
+                    pieFeaturesDeferred.await()
+                } catch (e: Exception) {
+                    Timber.e("Error loading pie features $e")
+                    null
+                }
+
+                val android10Features = try {
+                    android10FeaturesDeferred.await()
+                } catch (e: Exception) {
+                    Timber.e("Error loading android10 features $e")
+                    null
+                }
+
+                val versionFeatures =
+                    listOfNotNull(oreoFeatures, pieFeatures, android10Features)
+
+                uiState.value = UiState.Success(versionFeatures)
+
+            }
+        }
     }
 }
