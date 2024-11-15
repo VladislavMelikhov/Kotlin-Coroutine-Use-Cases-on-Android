94925a02bf933f0885399e0ed4a495cb22f51da5
Uladzislau Melikhau <vladislavmelikhov@gmail.com>
2024-11-15 16:35:19 +0100

    [Module-13] Terminal operator asLiveData()

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase1/FlowUseCase1ViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase1/FlowUseCase1ViewModel.kt
@@ -1,31 +1,27 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.flow.usecase1
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
-import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onStart
+import timber.log.Timber
 
 class FlowUseCase1ViewModel(
     stockPriceDataSource: StockPriceDataSource
 ) : BaseViewModel<UiState>() {
 
-    private val _currentStockPriceAsLiveData: MutableLiveData<UiState> = MutableLiveData()
-    val currentStockPriceAsLiveData: LiveData<UiState> = _currentStockPriceAsLiveData
-
-    init {
+    val currentStockPriceAsLiveData: LiveData<UiState> =
         stockPriceDataSource
             .latestStockList
             .map(UiState::Success)
             .onStart<UiState> {
                 emit(UiState.Loading)
             }
-            .onEach { uiState ->
-                _currentStockPriceAsLiveData.value = uiState
+            .onCompletion {
+                Timber.tag("StockPricesFlow").d("onCompletion")
             }
-            .launchIn(viewModelScope)
-    }
+            .asLiveData(viewModelScope.coroutineContext)
 }
