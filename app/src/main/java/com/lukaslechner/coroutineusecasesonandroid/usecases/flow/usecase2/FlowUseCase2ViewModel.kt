6f0a0c33f9989cb50b49775efcc47e773240e226
Uladzislau Melikhau <vladislavmelikhov@gmail.com>
2024-11-19 10:04:22 +0100

    [Module-13] Exercise: Intermediate Operators

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase2/FlowUseCase2ViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase2/FlowUseCase2ViewModel.kt
@@ -1,8 +1,27 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.flow.usecase2
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
+import com.lukaslechner.coroutineusecasesonandroid.usecases.flow.mock.Stock
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.take
+import kotlinx.coroutines.flow.withIndex
+import timber.log.Timber
+
+private const val GOOGLE_STOCK_NAME = "Alphabet (Google)"
+private const val GOOGLE_PRICE_THRESHOLD = 2300
+private const val US_COUNTRY_NAME = "United States"
+private const val BIGGEST_COMPANIES_RANK_THRESHOLD = 10
+private const val MAX_LIST_UPDATES_COUNT = 10
+private const val TAG = "exercise1"
 
 class FlowUseCase2ViewModel(
     stockPriceDataSource: StockPriceDataSource,
@@ -23,5 +42,65 @@ class FlowUseCase2ViewModel(
 
      */
 
-    val currentStockPriceAsLiveData: LiveData<UiState> = TODO()
+    val currentStockPriceAsLiveData: LiveData<UiState> =
+        stockPriceDataSource
+            .latestStockList
+            .filter { stocks ->
+                stocks.isGoogleHighPrice()
+            }
+            .map { stocks -> stocks
+                .filterUSCompanies()
+                .makeGoogleNumberOne()
+                .updateRanks()
+                .filterBiggestCompanies()
+            }
+            .take(MAX_LIST_UPDATES_COUNT)
+            .logEmissionIndex()
+            .flowOn(defaultDispatcher)
+            .map(UiState::Success)
+            .onStart<UiState> {
+                emit(UiState.Loading)
+            }
+            .asLiveData(viewModelScope.coroutineContext)
 }
+
+private fun List<Stock>.isGoogleHighPrice(): Boolean {
+    val google = firstOrNull { stock -> stock.name == GOOGLE_STOCK_NAME }
+    if (google == null) {
+        return false
+    }
+
+    val googlePrice = google.currentPrice
+    Timber.tag(TAG).d("googlePrice = $googlePrice")
+
+    return googlePrice > GOOGLE_PRICE_THRESHOLD
+}
+
+private fun List<Stock>.filterUSCompanies(): List<Stock> =
+    filter { stock ->
+        stock.country == US_COUNTRY_NAME
+    }
+
+private fun List<Stock>.makeGoogleNumberOne(): List<Stock> =
+    dropWhile { stock ->
+        stock.name != GOOGLE_STOCK_NAME
+    }
+
+private fun List<Stock>.filterBiggestCompanies(): List<Stock> =
+    filter { stock ->
+        stock.rank <= BIGGEST_COMPANIES_RANK_THRESHOLD
+    }
+
+private fun List<Stock>.updateRanks(): List<Stock> =
+    mapIndexed { index, stock ->
+        stock.copy(rank = index + 1)
+    }
+
+private fun <T> Flow<T>.logEmissionIndex(): Flow<T> =
+    withIndex()
+        .onEach { indexedValue ->
+            Timber.tag(TAG).d("emissionIndex = ${indexedValue.index}")
+        }
+        .map { indexedValue ->
+            indexedValue.value
+        }
