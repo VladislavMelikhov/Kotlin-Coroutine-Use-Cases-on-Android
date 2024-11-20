1dad43a80224292cba8b29109dad6b467f39c2b8
Uladzislau Melikhau <vladislavmelikhov@gmail.com>
2024-11-20 10:52:28 +0100

    [Module-14] Exercise: Flow Exception Handling

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase3/FlowUseCase3ViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase3/FlowUseCase3ViewModel.kt
@@ -3,10 +3,16 @@ package com.lukaslechner.coroutineusecasesonandroid.usecases.flow.usecase3
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.asLiveData
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.retry
+import retrofit2.HttpException
 import timber.log.Timber
+
+private const val TAG = "exercise2"
 
 class FlowUseCase3ViewModel(
     stockPriceDataSource: StockPriceDataSource
@@ -32,10 +38,24 @@ class FlowUseCase3ViewModel(
             UiState.Success(stockList) as UiState
         }
         .onStart {
+            Timber.tag(TAG).d("onStart")
             emit(UiState.Loading)
         }
         .onCompletion {
-            Timber.tag("Flow").d("Flow has completed.")
+            Timber.tag(TAG).d("onCompletion")
+        }
+        .retry { cause ->
+            Timber.tag(TAG).d("error: $cause")
+
+            val needToRetry = (cause is HttpException)
+            if (needToRetry) {
+                delay(5_000)
+                Timber.tag(TAG).d("retry")
+            }
+            needToRetry
+        }
+        .catch { e ->
+            emit(UiState.Error("Network request failed: $e"))
         }
         .asLiveData()
 }
