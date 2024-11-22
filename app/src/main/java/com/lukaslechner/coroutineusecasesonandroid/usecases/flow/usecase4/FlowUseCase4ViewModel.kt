e7d28908a920454e673dd5714feab4aa7f8fcbc3
Uladzislau Melikhau <vladislavmelikhov@gmail.com>
2024-11-22 08:12:44 +0100

    [Module-15] Converting Flows to SharedFlows with shareIn()

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase4/FlowUseCase4ViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase4/FlowUseCase4ViewModel.kt
@@ -1,10 +1,14 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.flow.usecase4
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.shareIn
 import timber.log.Timber
 
 private const val TAG = "ExposeFlowInVM"
@@ -13,7 +17,7 @@ class FlowUseCase4ViewModel(
     stockPriceDataSource: StockPriceDataSource
 ) : BaseViewModel<UiState>() {
 
-    val currentStockPrice: Flow<UiState> = stockPriceDataSource
+    val currentStockPrice: SharedFlow<UiState> = stockPriceDataSource
         .latestStockList
         .map(UiState::Success)
         .onStart<UiState> {
@@ -23,4 +27,8 @@ class FlowUseCase4ViewModel(
         .onCompletion {
             Timber.tag(TAG).d("Flow completed")
         }
+        .shareIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed()
+        )
 }
