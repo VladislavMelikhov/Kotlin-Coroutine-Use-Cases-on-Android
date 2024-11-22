888f29fdeec29946f4699b94b116304fbeb1119c
Uladzislau Melikhau <vladislavmelikhov@gmail.com>
2024-11-22 13:58:23 +0100

    [Module-15] Converting Flows to StateFlows with stateIn()

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase4/FlowUseCase4ViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase4/FlowUseCase4ViewModel.kt
@@ -2,13 +2,12 @@ package com.lukaslechner.coroutineusecasesonandroid.usecases.flow.usecase4
 
 import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onStart
-import kotlinx.coroutines.flow.shareIn
+import kotlinx.coroutines.flow.stateIn
 import timber.log.Timber
 
 private const val TAG = "ExposeFlowInVM"
@@ -17,19 +16,18 @@ class FlowUseCase4ViewModel(
     stockPriceDataSource: StockPriceDataSource
 ) : BaseViewModel<UiState>() {
 
-    val currentStockPrice: SharedFlow<UiState> = stockPriceDataSource
+    val currentStockPrice: StateFlow<UiState> = stockPriceDataSource
         .latestStockList
         .map(UiState::Success)
-        .onStart<UiState> {
+        .onStart {
             Timber.tag(TAG).d("Flow started")
-            emit(UiState.Loading)
         }
         .onCompletion {
             Timber.tag(TAG).d("Flow completed")
         }
-        .shareIn(
+        .stateIn(
             viewModelScope,
             SharingStarted.WhileSubscribed(5_000),
-            replay = 1,
+            UiState.Loading,
         )
 }
